9f7baf9fa7d2769301b80b45bdf1604a07002355
abdallah-hatem <ahkortam@gmail.com>
2023-06-26 19:01:04 +0300

    postgresql branch

--- Services/Config.js
+++ Services/Config.js
@@ -1,2 +1,2 @@
 export const ApiBaseUrl = "http://localhost:3000/api/";
-export const ApiBaseUrl2 = "https://good-tan-firefly-sari.cyclic.app/";
+export const ApiBaseUrl2 = "https://playstation-management-system2.vercel.app/";
--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -2,15 +2,14 @@ import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "../../../db";
 
 export async function GET() {
-  // const users = await prisma.user.findMany();
+  const users = await prisma.user.findMany();
 
-  // if (!users) {
-  //   return NextResponse.json(
-  //     { message: "ERROR! no users found" },
-  //     { status: 404 }
-  //   );
-  // }
+  if (!users) {
+    return NextResponse.json(
+      { message: "ERROR! no users found" },
+      { status: 404 }
+    );
+  }
 
-  // return NextResponse.json({ data: users }, { status: 200 });
-  return "";
+  return NextResponse.json({ data: users }, { status: 200 });
 }
